ebf10c31ff6d8b12ddee7512f0e3d7157decbf06
Svetlana Vyborova <lamborghini18@yandex.ru>
2021-02-04 16:18:04 +0300

    lesson53

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,10 @@ const Navbar = (props) => {
 				<NavLink to= "/profile" activeClassName = {s.activeLink}>Profile</NavLink>
 			</div>
 		<div className= {s.item}>
-				<NavLink to="/dialogs" activeClassName={s.activeLink}>Messedges</NavLink>
+				<NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
+			</div>
+		<div className= {s.item}>
+				<NavLink to="/users" activeClassName={s.activeLink}>Users</NavLink>
 			</div>
 		<div className={`${s.item} ${s.active}`}>
 				<NavLink to="/news" activeClassName={s.activeLink}>News</NavLink>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,52 +1,47 @@
+import * as axios from "axios";
 import React from "react";
-import styles from './Users.module.css'
+import styles from './Users.module.css';
+import userPhoto from '../../assets/images/user.png';
 
-let Users = (props) => {
-
-	if (props.users.length === 0) {
-		props.setUsers ( [
-			{
-				id: 1, photoUrl: 'http://avatarki-besplatno.ru/avatar/avatark/avatar286-1.jpg',
-				followed: true, fullName: 'Sveta', status: 'I am a boss', location: { city: 'Sochi', country: 'Russia' }
-			},
-			{
-				id: 2, photoUrl: 'http://avatarki-besplatno.ru/avatar/avatark/avatar286-1.jpg',
-				followed: false, fullName: 'Vlad', status: 'I am a boss', location: { city: 'Moscow', country: 'Russia' }
-			},
-			{
-				id: 3, photoUrl: 'http://avatarki-besplatno.ru/avatar/avatark/avatar286-1.jpg',
-				followed: false, fullName: 'Misha', status: 'I am a boss', location: { city: ' Sochi', country: 'Russia' }
-			},
-		],
-		)
+class Users extends React.Component {
+	constructor(props) {
+		super(props)
+		
+			axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+				this.props.setUsers(response.data.items)
+			});
 	}
 
+render() {
 	return <div>
-	{
-		props.users.map( u => <div key={u.id}>
-			<span>
-				<div>
-					<img src={u.photoUrl} className = {styles.userPhoto}/>
-				</div>
-				<duv>
-					{u.followed 
-						? <button onClick={() => { props.unfollow(u.id) } } >unFollow</button> 
-					: <button onClick = { () => { props.follow(u.id) } } >Follow</button> }
-					
-				</duv>
-			</span>
-			<span>
-				<div>{u.fullName}</div>
-				<div>{u.status}</div>
-			</span>
-			<span>
-				<div>{u.location.country}</div>
-				<div>{u.location.city}</div>
-			</span>
-		</div>)
-	}
+
+		{
+			this.props.users.map(u => <div key={u.id}>
+				<span>
+					<div>
+						<img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
+					</div>
+					<duv>
+						{u.followed
+							? <button onClick={() => { this.props.unfollow(u.id) }} >unFollow</button>
+							: <button onClick={() => { this.props.follow(u.id) }} >Follow</button>}
+
+					</duv>
+				</span>
+				<span>
+					<div>{u.name}</div>
+					<div>{u.status}</div>
+				</span>
+				<span>
+					<div>{'u.location.country'}</div>
+					<div>{'u.location.city'}</div>
+				</span>
+			</div>)
+		}
 	</div>
-	
 }
+
+}
+
 
 export default Users;
