3756496bff4c0932520a538d795d3ebf6bbcc259
Svetlana Vyborova <lamborghini18@yandex.ru>
2021-03-24 14:08:11 +0300

    less98 getting captcha

--- src/redux/auth_reducer.js
+++ src/redux/auth_reducer.js
@@ -1,7 +1,8 @@
 import { stopSubmit } from "redux-form";
-import { authAPI } from "../api/api";
+import { authAPI, securityAPI } from "../api/api";
 
 const SET_USER_DATA = "myApp/auth/SET_USER_DATA";
+const GET_CAPTCHA_URL_SUCCESS = "myApp/auth/GET_CAPTCHA_URL_SUCCESS";
 
 let initialState = {
   userId: null,
@@ -9,11 +10,13 @@ let initialState = {
   login: null,
   isFetching: false,
   isAuth: false,
+  captchaUrl: null, //if null then captcha is not required
 };
 
 const authReduсer = (state = initialState, action) => {
   switch (action.type) {
     case SET_USER_DATA:
+    case GET_CAPTCHA_URL_SUCCESS:
       return {
         ...state,
         ...action.payload,
@@ -28,6 +31,10 @@ export const setAuthUserData = (userId, email, login, isAuth) => ({
   type: SET_USER_DATA,
   payload: { userId, email, login, isAuth },
 });
+export const getCaptchaUrlSuccess = (captchaUrl) => ({
+  type: GET_CAPTCHA_URL_SUCCESS,
+  payload: { captchaUrl },
+});
 
 export const getAuthUserData = () => async (dispatch) => {
   const response = await authAPI.me();
@@ -37,10 +44,14 @@ export const getAuthUserData = () => async (dispatch) => {
   }
 };
 
-export const login = (email, password, rememberMe) => async (dispatch) => {
-  const response = await authAPI.login(email, password, rememberMe);
+export const login = (email, password, rememberMe, captcha) => async (
+  dispatch
+) => {
+  const response = await authAPI.login(email, password, rememberMe, captcha);
   if (response.data.resultCode === 0) {
     dispatch(getAuthUserData());
+  } else if (response.data.resultCode === 10) {
+    dispatch(getCaptcha());
   } else {
     const message =
       response.data.messages.length > 0
@@ -49,6 +60,13 @@ export const login = (email, password, rememberMe) => async (dispatch) => {
     dispatch(stopSubmit("login", { _error: message }));
   }
 };
+
+export const getCaptcha = () => async (dispatch) => {
+  const response = await securityAPI.getCaptchaUrl();
+  const captchaUrl = response.data.url;
+  dispatch(getCaptchaUrlSuccess(captchaUrl));
+};
+
 export const logout = () => async (dispatch) => {
   const response = await authAPI.logout();
   if (response.data.resultCode === 0) {
